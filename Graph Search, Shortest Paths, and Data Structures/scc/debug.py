0498086db55117916d1be0fb8d420d668d55bad7
Ronald Rey <reyronald@gmail.com>
2017-04-06 16:20:21 -0400

    SCC adding all test cases

--- Graph Search, Shortest Paths, and Data Structures/scc/debug.py
+++ Graph Search, Shortest Paths, and Data Structures/scc/debug.py
@@ -15,10 +15,24 @@ GRAPH = {
   2: [8]
 }
 
-#result = Graph.create_from_dict(GRAPH).find_scc()
-#assert result == '3,3,3,0,0'
+result = Graph.create_from_dict(GRAPH).find_scc()
+assert result == '3,3,3,0,0'
 
-result = Graph.create_from_file("D:/repos/playground/Graph Search, Shortest Paths, and Data Structures/scc/tc3.txt").find_scc()
+root = "D:/repos/playground/Graph Search, Shortest Paths, and Data Structures/scc/"
+
+result = Graph.create_from_file(root + "tc1.txt").find_scc()
+assert result == '3,3,3,0,0'
+
+result = Graph.create_from_file(root + "tc2.txt").find_scc()
+assert result == '3,3,2,0,0'
+
+result = Graph.create_from_file(root + "tc3.txt").find_scc()
 assert result == '3,3,1,1,0'
+
+result = Graph.create_from_file(root + "tc4.txt").find_scc()
+assert result == '7,1,0,0,0'
+
+result = Graph.create_from_file(root + "tc5.txt").find_scc()
+assert result == '6,3,2,1,0'
 
 print result
