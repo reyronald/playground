334ffe68beb4251e4341540d559d67c5f2a70f4a
Ronald Rey <reyronald@gmail.com>
2017-04-20 23:46:56 -0400

    SCC. Adding more test cases

--- Graph Search, Shortest Paths, and Data Structures/scc/debug.py
+++ Graph Search, Shortest Paths, and Data Structures/scc/debug.py
@@ -1,9 +1,9 @@
 # graph representations:
 # http://www.cs.princeton.edu/courses/archive/fall07/cos226/lectures/digraph.pdf
 
-from scc.graph2 import Graph
+from graph2 import Graph
 
-ROOT = "D:/repos/playground/Graph Search, Shortest Paths, and Data Structures/scc/"
+ROOT = "e:/repos/playground/Graph Search, Shortest Paths, and Data Structures/scc/"
 
 RESULT = Graph.create_from_file(ROOT + "tc1.txt").find_scc()
 assert RESULT == '3,3,3,0,0'
@@ -14,13 +14,24 @@ assert RESULT == '3,3,2,0,0'
 RESULT = Graph.create_from_file(ROOT + "tc3.txt").find_scc()
 assert RESULT == '3,3,1,1,0'
 
-RESULT = Graph.create_from_file(ROOT + "tc3.txt").find_scc()
-assert RESULT == '3,3,1,1,0'
-
 RESULT = Graph.create_from_file(ROOT + "tc4.txt").find_scc()
 assert RESULT == '7,1,0,0,0'
 
 RESULT = Graph.create_from_file(ROOT + "tc5.txt").find_scc()
 assert RESULT == '6,3,2,1,0'
+
+####
+
+RESULT = Graph.create_from_file(ROOT + "tc6.txt").find_scc()
+assert RESULT == '3,1,1,0,0'
+
+RESULT = Graph.create_from_file(ROOT + "tc7.txt").find_scc()
+assert RESULT == '7,3,0,0,0'
+
+RESULT = Graph.create_from_file(ROOT + "tc8.txt").find_scc()
+assert RESULT == '4,2,1,1,1'
+
+RESULT = Graph.create_from_file(ROOT + "tc9.txt").find_scc()
+assert RESULT == '9,1,1,1,1'
 
 print RESULT
