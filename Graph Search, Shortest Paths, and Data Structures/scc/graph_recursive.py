54b12081a0d1fa724336e542a9d9458a86e0616b
Ronald Rey <reyronald@gmail.com>
2017-04-21 15:00:31 -0400

    SCC. Code rearrangement and further reuse in graph_recursive

--- Graph Search, Shortest Paths, and Data Structures/scc/graph_recursive.py
+++ Graph Search, Shortest Paths, and Data Structures/scc/graph_recursive.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.graph = defaultdict(set)
-        self.reversed_graph = defaultdict(set)
         self.s = None
         self.f = []
         self.t = 0
@@ -11,57 +10,60 @@ class Graph:
         self.scc_count = defaultdict(lambda: 0)
 
     def add_edge(self, tail, head):
-        self.reversed_graph[head].add(tail)
+        self.graph[head].add(tail)
 
-        if self.reversed_graph[tail] is None:
-            self.reversed_graph[tail] = set()
+        if self.graph[tail] is None:
+            self.graph[tail] = set()
 
     def find_scc(self):
-        self.f = [0]*(len(self.reversed_graph) + 1)
-        self.dfs_loop_reverse()
-        self.dfs_loop_second()
+        # First DFS Pass
+        self.f = [0]*(len(self.graph) + 1)
+        self.dfs_loop(lambda: [(yield vertex) for vertex in self.graph])
+
+        # Second DFS Pass
+        self.traspose_graph()
+        finishing_order = self.finishing_order[::-1]
+        f = self.f[:]
+        self.scc_count = defaultdict(lambda: 0)
+        self.dfs_loop(lambda: [(yield f[vertex]) for vertex in finishing_order])
+
+        # Calculating Result
         sccs = self.scc_count.values()
         sccs.sort(reverse=True)
         del sccs[5:]
         sccs = sccs + [0]*(5-len(sccs))
         return ','.join(map(str, sccs))
 
-    def dfs_loop_reverse(self):
+    def dfs_loop(self, get_vector):
         explored = set()
-        for vertex in self.reversed_graph:
+        for vertex in get_vector():
             if vertex not in explored:
-                self.dfs_reverse_recur(vertex, explored)
-
-    def dfs_loop_second(self):
-        self.traspose_graph()
-        explored = set()
-        finishing_order = self.finishing_order[:]
-        f = self.f[:]
-        while finishing_order:
-            vertex = f[finishing_order.pop()]
-            if vertex not in explored:
+                # Only relevant for second DFS Pass
                 self.s = vertex
-                self.dfs_reverse_recur(vertex, explored)
+                self.dfs_recursive(vertex, explored)
 
-    def dfs_reverse_recur(self, start, explored):
+    def dfs_recursive(self, start, explored):
         explored.add(start)
 
+        # Only relevant for second DFS Pass
         if self.s:
             self.scc_count[self.s] = self.scc_count[self.s] + 1
 
-        for neighbor in self.reversed_graph[start]:
+        for neighbor in self.graph[start]:
             if neighbor not in explored:
-                self.dfs_reverse_recur(neighbor, explored)
+                self.dfs_recursive(neighbor, explored)
 
+        # Only relevant for first DFS Pass
         self.t += 1
         self.f[start] = self.t
         self.finishing_order.append(start)
 
     def traspose_graph(self):
-        for vertex in self.reversed_graph:
-            for neighbor in self.reversed_graph[vertex]:
-                self.graph[self.f[neighbor]].add(self.f[vertex])
-        self.reversed_graph = self.graph
+        temp_graph = defaultdict(set)
+        for vertex in self.graph:
+            for neighbor in self.graph[vertex]:
+                temp_graph[self.f[neighbor]].add(self.f[vertex])
+        self.graph = temp_graph
 
 ######################################
 
