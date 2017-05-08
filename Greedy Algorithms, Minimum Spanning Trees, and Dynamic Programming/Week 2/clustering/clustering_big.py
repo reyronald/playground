17eb894f5fd9beaf8fc6de45e7d524279700071f
Ronald Rey <reyronald@gmail.com>
2017-05-08 15:07:10 -0400

    Fixing error in clustering_big

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 2/clustering/clustering_big.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 2/clustering/clustering_big.py
@@ -2,12 +2,14 @@ from collections import defaultdict
 
 class DisjointSet:
     def __init__(self):
+        self.cluster_amount = 0
         self.parents = defaultdict(dict)
 
     def in_set(self, value):
         return value in self.parents
 
     def make_set(self, value):
+        self.cluster_amount += 1
         self.parents[value] = {'parent': value, 'rank':0}
 
     def find(self, value):
@@ -22,13 +24,17 @@ class DisjointSet:
         if x_root == y_root:
             return
 
+        self.cluster_amount -= 1
+
         if self.parents[x_root]['rank'] < self.parents[y_root]['rank']:
             self.parents[x_root]['parent'] = y_root
-        elif self.parents[x_root]['rank'] > self.parents[y_root]['rank']:
-            self.parents[y_root]['parent'] = x_root
         else:
             self.parents[y_root]['parent'] = x_root
-            self.parents[x_root]['rank'] = self.parents[x_root]['rank'] + 1
+            if self.parents[x_root]['rank'] == self.parents[y_root]['rank']:
+                self.parents[x_root]['rank'] = self.parents[x_root]['rank'] + 1
+
+    def get_cluster_size(self):
+        return self.cluster_amount
 
 def read_input(path):
     vertices_int = set()
@@ -63,7 +69,7 @@ def get_answer(path):
                 if not disjointset.in_set(bitwise_result):
                     disjointset.make_set(bitwise_result)
                 disjointset.union(vertex, bitwise_result)
-    return len(set([item['parent'] for item in disjointset.parents.itervalues()]))
+    return disjointset.get_cluster_size()
 
 def test_case_1():
     path = "D:\\repos\playground\Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming\Week 2\clustering\\tc1_answer_3.txt"
@@ -74,7 +80,7 @@ def test_case_2():
     path = "D:\\repos\playground\Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming\Week 2\clustering\\tc2_answer_2.txt"
     answer = get_answer(path)
     assert answer == 2
-    
+
 def test_case_3():
     path = "D:\\repos\playground\Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming\Week 2\clustering\\tc3_answer_6.txt"
     answer = get_answer(path)
@@ -83,7 +89,7 @@ def test_case_3():
 def assignment():
     path = "D:\\repos\playground\Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming\Week 2\clustering\clustering_big.txt"
     answer = get_answer(path)
-    assert answer == 13172 # 13172 (9 seconds execution time)
+    assert answer == 6118 # 6118 or 13172 (8.93056747874 seconds execution time)
     print answer
     return answer 
 
