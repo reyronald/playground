2c5a4ac68a7f929313ebb30dd0c53a38abe061f0
Ronald Rey <reyronald@gmail.com>
2017-05-11 14:30:29 -0400

    Week 3 - Huffman code cleaning and refactoring

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 3/huffman/huffman.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 3/huffman/huffman.py
@@ -1,6 +1,6 @@
 from heapq import heappush, heappop, heapify
 
-class Character:
+class HuffmanTreeNode:
     def __init__(self, label, weight, left = None, right = None):
         self.label = str(label)
         self.weight = weight
@@ -13,144 +13,131 @@ class Character:
     def is_leaf(self):
         return self.left is None and self.right is None
 
-class HuffmanTree:
-    def __init__(self, input):
-        ordered_input = [(c.weight, c) for c in input]
-        heapify(ordered_input)
-        self.ordered_input = ordered_input
+class HuffmanCode:
+    def __init__(self, alphabet):
+        alphabet_heap = [(c.weight, c) for c in alphabet]
+        heapify(alphabet_heap)
+        self.alphabet_heap = alphabet_heap
         self.tree = None
         self.codeword_map = None
         self.max_codeword_length = None
         self.min_codeword_length = None
 
-    def generate_huffman_tree(self):
-        self.tree = HuffmanTree.huffman(self.ordered_input)
-        self.codeword_map = self._walk_tree(self.tree)
+    def generate_huffman_coding(self):
+        self.tree = HuffmanCode._huffman(self.alphabet_heap)
+        self.codeword_map = self._generate_codeword_map(self.tree)
         return self
 
-    def _walk_tree(self, node, order = 0, result = {}):
+    @staticmethod
+    def _huffman(alphabet_heap):
+        while len(alphabet_heap) > 1:
+            left = heappop(alphabet_heap)[1]
+            right = heappop(alphabet_heap)[1]
+
+            top_label = left.label + ',' + right.label
+            top_weight = left.weight + right.weight
+            top = HuffmanTreeNode(top_label, top_weight, left, right)
+
+            heappush(alphabet_heap, (top.weight, top))
+        return heappop(alphabet_heap)[1]
+
+    def _generate_codeword_map(self, node, order=0, codeword_map=None):
         if node.is_leaf():
-            result[node.label] = order
+            if codeword_map is None:
+                codeword_map = {}
+            codeword_map[node.label] = order
             if self.max_codeword_length is None or order > self.max_codeword_length:
                 self.max_codeword_length = order
             if self.min_codeword_length is None or order < self.min_codeword_length:
                 self.min_codeword_length = order
 
         if node.left is not None:
-            self._walk_tree(node.left, order + 1)
+            self._generate_codeword_map(node.left, order + 1)
         if node.right is not None:
-            self._walk_tree(node.right, order + 1)
+            self._generate_codeword_map(node.right, order + 1)
 
-        return result
-
-    @staticmethod
-    def huffman_recursive(ordered_input):
-        if not ordered_input or len(ordered_input) == 1:
-            return heappop(ordered_input)[1]
-
-        first = heappop(ordered_input)[1]
-        second = heappop(ordered_input)[1]
-
-        top = Character(first.label + ',' + second.label, first.weight + second.weight, first, second)
-
-        heappush(ordered_input, (top.weight, top))
-
-        return HuffmanTree.huffman(ordered_input)
-
-    @staticmethod
-    def huffman(ordered_input):
-        while True:
-            if len(ordered_input) == 1:
-                return heappop(ordered_input)[1]
-
-            first = heappop(ordered_input)[1]
-            second = heappop(ordered_input)[1]
-
-            top = Character(first.label + ',' + second.label, first.weight + second.weight, first, second)
-
-            heappush(ordered_input, (top.weight, top))
+        return codeword_map
 
 def test_case_0():
-    input = set([
-        Character('A', 3),
-        Character('B', 2),
-        Character('C', 6),
-        Character('D', 8),
-        Character('E', 2),
-        Character('F', 6),
-    ])
-
-    tree = HuffmanTree(input).generate_huffman_tree()
-    assert tree.min_codeword_length == 2
-    assert tree.max_codeword_length == 4
+    alphabet = [
+        HuffmanTreeNode('A', 3),
+        HuffmanTreeNode('B', 2),
+        HuffmanTreeNode('C', 6),
+        HuffmanTreeNode('D', 8),
+        HuffmanTreeNode('E', 2),
+        HuffmanTreeNode('F', 6),
+    ]
+    huffman_code = HuffmanCode(alphabet).generate_huffman_coding()
+    assert huffman_code.min_codeword_length == 2
+    assert huffman_code.max_codeword_length == 4
 
 def test_case_1():
-    input = [
-        Character(1, 5),
-        Character(1, 25),
-        Character(1, 32),
-        Character(1, 20),
-        Character(1, 18),
+    alphabet = [
+        HuffmanTreeNode(1, 5),
+        HuffmanTreeNode(1, 25),
+        HuffmanTreeNode(1, 32),
+        HuffmanTreeNode(1, 20),
+        HuffmanTreeNode(1, 18),
     ]
 
-    tree = HuffmanTree(input).generate_huffman_tree()
-    assert tree.min_codeword_length == 2
-    assert tree.max_codeword_length == 3
+    huffman_code = HuffmanCode(alphabet).generate_huffman_coding()
+    assert huffman_code.min_codeword_length == 2
+    assert huffman_code.max_codeword_length == 3
 
 def test_case_2():
-    input = [
-        Character(1, 10),
-        Character(2, 37),
-        Character(3, 59),
-        Character(4, 43),
-        Character(5, 27),
-        Character(6, 30),
-        Character(7, 96),
-        Character(8, 96),
-        Character(9, 71),
-        Character(10, 8),
-        Character(11, 76),
+    alphabet = [
+        HuffmanTreeNode(1, 10),
+        HuffmanTreeNode(2, 37),
+        HuffmanTreeNode(3, 59),
+        HuffmanTreeNode(4, 43),
+        HuffmanTreeNode(5, 27),
+        HuffmanTreeNode(6, 30),
+        HuffmanTreeNode(7, 96),
+        HuffmanTreeNode(8, 96),
+        HuffmanTreeNode(9, 71),
+        HuffmanTreeNode(10, 8),
+        HuffmanTreeNode(11, 76),
     ]
-    tree = HuffmanTree(input).generate_huffman_tree()
-    assert tree.min_codeword_length == 2
-    assert tree.max_codeword_length == 6
+    huffman_code = HuffmanCode(alphabet).generate_huffman_coding()
+    assert huffman_code.min_codeword_length == 2
+    assert huffman_code.max_codeword_length == 6
 
 def test_case_3():
-    input = [
-        Character(1, 15),
-        Character(2, 895),
-        Character(3, 121),
-        Character(4, 188),
-        Character(5, 953),
-        Character(6, 378),
-        Character(7, 849),
-        Character(8, 153),
-        Character(9, 579),
-        Character(10, 144),
-        Character(11, 727),
-        Character(12, 589),
-        Character(13, 301),
-        Character(14, 442),
-        Character(15, 327),
-        Character(16, 930),
+    alphabet = [
+        HuffmanTreeNode(1, 15),
+        HuffmanTreeNode(2, 895),
+        HuffmanTreeNode(3, 121),
+        HuffmanTreeNode(4, 188),
+        HuffmanTreeNode(5, 953),
+        HuffmanTreeNode(6, 378),
+        HuffmanTreeNode(7, 849),
+        HuffmanTreeNode(8, 153),
+        HuffmanTreeNode(9, 579),
+        HuffmanTreeNode(10, 144),
+        HuffmanTreeNode(11, 727),
+        HuffmanTreeNode(12, 589),
+        HuffmanTreeNode(13, 301),
+        HuffmanTreeNode(14, 442),
+        HuffmanTreeNode(15, 327),
+        HuffmanTreeNode(16, 930),
     ]
-    tree = HuffmanTree(input).generate_huffman_tree()
-    assert tree.min_codeword_length == 3
-    assert tree.max_codeword_length == 7
+    huffman_code = HuffmanCode(alphabet).generate_huffman_coding()
+    assert huffman_code.min_codeword_length == 3
+    assert huffman_code.max_codeword_length == 7
 
 def assignment():
     node_number = 1
-    input = []
+    alphabet = []
     with open("D:\\repos\playground\Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming\Week 3\huffman\huffman.txt") as fin:
         next(fin)
         for line in fin:
             if not line:
                 break
-            input.append(Character(node_number, int(line.rstrip())))
+            alphabet.append(HuffmanTreeNode(node_number, int(line.rstrip())))
             node_number += 1
-    tree = HuffmanTree(input).generate_huffman_tree()
-    assert tree.min_codeword_length == 9
-    assert tree.max_codeword_length == 19
+    huffman_code = HuffmanCode(alphabet).generate_huffman_coding()
+    assert huffman_code.min_codeword_length == 9
+    assert huffman_code.max_codeword_length == 19
 
 def main():
     test_case_0()
